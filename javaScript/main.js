2d80f5693787ebd9320a40f134da87504b740b82
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-02-22 23:33:51 -0300

    commit 3

---
+++ javaScript/main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".box1__cont3__menu")
+const nav = document.querySelector(".nav2");
+const nav_2 = document.querySelector(".nav2__list");
+
+
+menu.addEventListener("click",()=>{
+    /* nav.style.height = "100%"; */
+    nav.classList.toggle("mostrar");
+})
+
+/* menu.addEventListener("click",()=>{
+    nav_2.classList.toggle("mostrar");
+}) */
