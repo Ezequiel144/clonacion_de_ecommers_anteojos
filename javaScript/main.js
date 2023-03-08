1d3e2f77d9eda7c1795133c7d553d5f286f20d53
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-03-08 14:49:38 -0300

    commit 16

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,12 @@
-const menu = document.querySelector(".box1__cont3__menu")
+const menu = document.querySelector(".box1__cont3__menu");
 const nav = document.querySelector(".nav2");
 const nav_2 = document.querySelector(".nav2__list");
+const cart = document.querySelector(".box1__cont2__cart")
+const cartdiv = document.querySelector(".cartdiv")
+const cartX = document.querySelector(".cartdiv__x");
+const clickcart = document.querySelector("#cart1");
+const cantidad = document.querySelector(".cartdiv__content__cont__number")
+let cont = 0;
 
 
 menu.addEventListener("click",()=>{
@@ -8,6 +14,17 @@ menu.addEventListener("click",()=>{
     nav.classList.toggle("mostrar");
 })
 
-/* menu.addEventListener("click",()=>{
-    nav_2.classList.toggle("mostrar");
-}) */
+cart.addEventListener("click",()=>{
+    cartdiv.style.display = "block";
+    
+})
+
+cartX.addEventListener("click",()=>{
+    cartdiv.style.display = "none";
+})
+
+clickcart.addEventListener("click",()=>{
+    cont++;
+    cartdiv.style.display = "block";
+    cantidad.value = cont;
+})
